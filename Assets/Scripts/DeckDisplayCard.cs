823346819deb7caa035914a66c60361f7ef54afb
Chenxu Song <songchenxu07@gmail.com>
2022-05-04 11:35:30 -0700

    starting on deckbuilder

---
+++ Assets/Scripts/DeckDisplayCard.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckDisplayCard : MonoBehaviour
+{
+    public string cardName;
+    public Sprite art;
+    public string description;
+    public int cost;
+    public int attack;
+    public int health;
+    public string faction;
+    public GameObject cardAbility; // We'll probably have to write prefabs with the card ability and instantiate them on activation
+    public DeckBuilder.card_type type;
+    // Other potential stats
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

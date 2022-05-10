24375ea24259ee78a516bd7b174ed82004b9a21e
Thomas Nguyen <ngutn24@gmail.com>
2022-05-10 00:04:22 -0700

    Added pass turn highlight and free pass indicator, added rough art for guard, renamed guard to captain

--- Assets/Scripts/LoadingController.cs
+++ Assets/Scripts/LoadingController.cs
@@ -8,6 +8,11 @@ public class LoadingController : MonoBehaviour
 {
     public static CapstoneLogger LOGGER;
 
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
